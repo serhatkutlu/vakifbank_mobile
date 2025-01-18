4220afb5c122e6ea2d30cc32c6fa73e117649889
serhat kutlu <m.serhat.kutlu@gmail.com>
2025-01-18 19:40:09 +0300

    Feat: Created 'data' module, added repository implementation, mapper, and DI structur

---
+++ core/data/src/main/java/com/example/data/repository/exchangerates/implementation/ExchangeRatesRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.example.data.repository.exchangerates.implementation
+
+import com.example.data.mapper.toRateUiData
+import com.example.data.repository.BaseRepository
+import com.example.common.resource.Resource
+import com.example.common.resource.transform
+import com.example.datasource.exchangedataSource.abstraction.ExchangeRatesDataSource
+import com.example.domain.model.exchangerate.RateUiData
+import com.example.domain.repository.abstraction.ExchangeRatesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class ExchangeRatesRepositoryImpl @Inject constructor(private val exchangeRatesDataSource: ExchangeRatesDataSource):
+    ExchangeRatesRepository, BaseRepository() {
+    override suspend fun getExchangeRatesData(): Flow<Resource<RateUiData>> {
+        return safeApiCall {
+            exchangeRatesDataSource.getExchangeRatesData()
+        }.map { resource ->
+            resource.transform {
+                Resource.Success(it?.rates?.toRateUiData())
+            }
+        }
+    }
+}
