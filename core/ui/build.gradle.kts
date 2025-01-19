ab8c699376992af7f10f5d8e6562dbacf205973e
serhat kutlu <m.serhat.kutlu@gmail.com>
2025-01-19 14:49:10 +0300

    feat:ui modul created and BaseFragment added

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,22 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("com.google.dagger.hilt.android")
-    id ("kotlin-kapt")
 }
 
 android {
-    namespace = "com.example.vakifbank_mobile"
+    namespace = "com.example.ui"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.vakifbank_mobile"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -34,7 +29,6 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
-
     }
     buildFeatures {
         viewBinding = true
@@ -43,19 +37,10 @@ android {
 
 dependencies {
 
-    implementation(project(":core:network"))
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-
-
-    implementation (libs.hilt.android)
-    kapt (libs.hilt.compiler)
 }
