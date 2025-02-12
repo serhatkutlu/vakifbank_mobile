f42c37ea33e60613e90d9339e0757deb385aad7a
serhat kutlu <m.serhat.kutlu@gmail.com>
2025-02-12 13:54:04 +0300

    feat: added new functionalities to core module

--- core/common/src/main/java/com/example/common/extension/Extension.kt
+++ core/common/src/main/java/com/example/common/extension/Extension.kt
@@ -1,17 +1,51 @@
 package com.example.common.extension
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Configuration
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
+import com.example.common.helper.LocaleHelper
+import org.intellij.lang.annotations.Language
+import java.util.Locale
 
 object Extension {
 
+    @SuppressLint("MissingPermission")
     fun Context.isInternetAvailable(): Boolean {
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork: Network? = connectivityManager.activeNetwork
         val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
 
         return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+    }
+
+    fun Context.changeAppLanguage(languageCode: Language) {
+        LocaleHelper.setLocale(this, languageCode.code)
+    }
+
+    fun Context.getAppLanguage(): Language {
+        val locale: Locale = this.resources.configuration.locale
+        return when (locale.language) {
+            "en" -> Language.ENGLISH
+            "tr" -> Language.TURKISH
+            else -> Language.ENGLISH
+        }
+
+    }
+
+
+    enum class Language(val code: String) {
+        ENGLISH("en"),
+        TURKISH("tr");
+
+        fun getNextLanguage(): Language {
+            return when (this) {
+                ENGLISH -> TURKISH
+                TURKISH -> ENGLISH
+            }
+        }
     }
 }
--- core/ui/src/main/java/com/example/ui/extensions/extension.kt
+++ core/ui/src/main/java/com/example/ui/extensions/extension.kt
@@ -2,7 +2,12 @@ package com.example.ui.extensions
 
 import android.view.View
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.launch
 
 object extension {
 
@@ -22,6 +27,13 @@ object extension {
 
     fun View.invisible(){
         this.visibility=View.INVISIBLE
+    }
+    fun Fragment.launchAndRepeatWithViewLifecycle(block: suspend () -> Unit) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                block()
+            }
+        }
     }
 
 }
